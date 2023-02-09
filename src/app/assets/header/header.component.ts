f7d365fd434fbd5fc7573078d72f17e9ca6a35e4
6fy <97406109+6fy@users.noreply.github.com>
2023-02-09 20:28:25 +0100

    responsiveness

--- src/app/assets/header/header.component.ts
+++ src/app/assets/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+	private visible: boolean = false;
+
 	public items: Array<MenuItem> = [
 		{ title: 'Home', url: '/', active: true, },
 		{ title: 'Dames', url: '/', active: false, },
@@ -18,7 +20,20 @@ export class HeaderComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit(): void {
+		this.applyVisibility();
 	} 
+
+	public toggleHamburgerMenu() {
+		this.visible = !this.visible;
+		this.applyVisibility();
+	}
+
+	private applyVisibility() {
+		const hamburgerMenu = document.getElementById('hamburgerItems');
+		if (!hamburgerMenu) return console.error('Hamburger menu not found');
+
+		hamburgerMenu.style.display = this.visible ? 'block' : 'none';
+	}
 
 }
 
